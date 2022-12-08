26a36791fae25e2c41cec9f25f08f9316ee250b5
Richard Reynard <richard.reynard@my.jcu.edu.au>
2022-12-08 23:40:51 +0800

    Initial commit.

---
+++ WeeklyExercise/BabyNameComparison.java
@@ -0,0 +1,24 @@
+// BabyNameComparison.java
+// Ask user for three first names and concatenates them in every possible two name combination
+import java.util.*;
+
+public class BabyNameComparison {
+    public static void main(String[] args)
+    {
+        String fname1, fname2, fname3;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a first name: ");
+        fname1 = in.nextLine();
+        System.out.print("Enter the second first name: ");
+        fname2 = in.nextLine();
+        System.out.print("Enter the third first name: ");
+        fname3 = in.nextLine();
+        System.out.println("The possible name combinations are:");
+        System.out.println(fname1+ " " +fname2);
+        System.out.println(fname1+ " " +fname3);
+        System.out.println(fname2+ " " +fname1);
+        System.out.println(fname2+ " " +fname3);
+        System.out.println(fname3+ " " +fname1);
+        System.out.println(fname3+ " " +fname2);
+    }
+}
